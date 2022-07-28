4446a27de1e83c37aebf01cfa794eea4e87b14d7
chenmh <chenmh@sucsoft.com>
2022-07-28 11:11:21 +0800

    fix: wrong name

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -14,7 +14,7 @@ const routes: RouteRecordRaw[] = [
     },
     {
         path: '/mobile',
-        name: 'mobile',
+        name: 'Mobile',
         component: () => import("@/views/Mobile.vue")
     },
 ]
