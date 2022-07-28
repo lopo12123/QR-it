c8aad5f1901713bde4ab20e4e6d824e8be054c3f
chenmh <chenmh@sucsoft.com>
2022-07-28 15:24:40 +0800

    feat: limit of text

--- src/views/Desktop.vue
+++ src/views/Desktop.vue
@@ -1,20 +1,36 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import { OutputType } from "@/scripts";
-import { toCanvas } from "qrcode";
+import { readonly, ref } from "vue";
+import { QRCodeRenderersOptions, toCanvas } from "qrcode";
 
-const targetType = ref<OutputType>('text')
+const targetType = ref<'text' | 'url'>('url')
 
-// region 文本
+// region source
+// 文本
+const textLimit = ref({
+    L: 900,
+    M: 700,
+    Q: 550,
+    H: 400
+})
 const textSource = ref('')
-// endregion
-
-// region url
+// url
 const protocol = ref<'https' | 'http'>('https')
 const address = ref('')
 // endregion
 
-// region 渲染二维码
+// region custom options
+const configurationVisible = ref(true)
+const darkColor = ref('#000000')
+const lightColor = ref('#ffffff')
+const strokeWidth = ref(4)
+const corrLevel = ref<'L' | 'M' | 'Q' | 'H'>('M')
+const changeCorrLevel = (to: 'L' | 'M' | 'Q' | 'H') => {
+    corrLevel.value = to
+    textSource.value = textSource.value.slice(0, textLimit.value[to])
+}
+// endregion
+
+// region render
 const outputContainer = ref<HTMLCanvasElement>()
 const drawFail = (text?: string) => {
     const cvs = outputContainer.value
@@ -33,10 +49,19 @@ const toQR = () => {
     const cvs = outputContainer.value
     if(!cvs) return
 
+    const options: QRCodeRenderersOptions = {
+        errorCorrectionLevel: corrLevel.value,
+        color: {
+            dark: darkColor.value,
+            light: lightColor.value
+        },
+        margin: strokeWidth.value
+    }
+
     switch(targetType.value) {
         case 'text':
             if(textSource.value.trim() === '') drawFail('Empty Input!')
-            else toCanvas(cvs, textSource.value ?? '')
+            else toCanvas(cvs, textSource.value ?? '', options)
                 .catch(_ => {
                     console.log(_)
                     drawFail()
@@ -44,7 +69,7 @@ const toQR = () => {
             break
         case 'url':
             if(address.value.trim() === '') drawFail('Empty Input!')
-            else toCanvas(cvs, `${ protocol.value }://${ address.value }`)
+            else toCanvas(cvs, `${ protocol.value }://${ address.value }`, options)
                 .catch(_ => {
                     console.log(_)
                     drawFail()
@@ -56,7 +81,7 @@ const toQR = () => {
 </script>
 
 <template>
-    <div class="desktop-view">
+    <div class="desktop-view with-scroll">
         <div class="output-selector">
             <div :class="targetType === 'text' ? 'active' : 'default'"
                  @click="targetType = 'text'">
@@ -69,13 +94,12 @@ const toQR = () => {
         </div>
 
         <!--region source-box-->
-        <div class="text-source">
+        <div class="text-source" v-if="targetType === 'text'">
             <div style="margin-bottom: 0.5rem">
                 <b>words limit:</b>&nbsp;
-                <i>{{ textSource.length }} / 700</i>
+                <i>{{ textSource.length }} / {{ textLimit[corrLevel] }}</i>
             </div>
-            <textarea v-if="targetType === 'text'" class="text-box"
-                      v-model="textSource" maxlength="700"/>
+            <textarea class="text-box" v-model="textSource" :maxlength="textLimit[corrLevel]"/>
         </div>
 
         <div class="url-source" v-if="targetType === 'url'">
@@ -91,6 +115,54 @@ const toQR = () => {
             </div>
 
             <input class="address-box" type="text" v-model="address">
+        </div>
+        <!-- endregion -->
+
+        <!-- region custom -->
+        <div class="custom-options">
+            <div :class="configurationVisible ? 'configure-active' : 'configure-default'"
+                 @click="configurationVisible = !configurationVisible">
+                More Configuration
+            </div>
+            <div class="configure-box"
+                 :style="configurationVisible
+                 ? 'height: 12rem; margin-top: 1rem; border: solid 1px #7b7b7b;'
+                 : 'height: 0; margin: 0; border: none;'">
+                <div class="conf-line">
+                    <span class="conf-label">dark area color</span>
+                    <input class="color-picker" type="color" v-model="darkColor">
+                </div>
+                <div class="conf-line">
+                    <span class="conf-label">light area color</span>
+                    <input class="color-picker" type="color" v-model="lightColor">
+                </div>
+                <div class="conf-line">
+                    <span class="conf-label">quiet zone width</span>
+                    <input class="slider" type="range" v-model="strokeWidth" min="0" max="20" step="1">
+                    <span style="margin-left: 0.5rem">{{ strokeWidth }}</span>
+                </div>
+                <div class="conf-line">
+                    <span class="conf-label">error correction</span>
+                    <div class="radios">
+                        <span :class="corrLevel === 'L' ? 'radio-active' : 'radio-default'"
+                              @click="changeCorrLevel('L')">
+                            low
+                        </span>
+                        <span :class="corrLevel === 'M' ? 'radio-active' : 'radio-default'"
+                              @click="changeCorrLevel('M')">
+                            medium
+                        </span>
+                        <span :class="corrLevel === 'Q' ? 'radio-active' : 'radio-default'"
+                              @click="changeCorrLevel('Q')">
+                            quartile
+                        </span>
+                        <span :class="corrLevel === 'H' ? 'radio-active' : 'radio-default'"
+                              @click="changeCorrLevel('H')">
+                            high
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- endregion -->
 
@@ -110,6 +182,7 @@ const toQR = () => {
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
+    overflow: auto;
 
     .output-selector {
         position: relative;
@@ -229,6 +302,117 @@ const toQR = () => {
     }
 
     // endregion
+
+    .custom-options {
+        position: relative;
+        width: 100%;
+        height: fit-content;
+        margin: 1rem 0;
+
+        %configure-btn {
+            position: relative;
+            width: 12rem;
+            height: 2rem;
+            border: solid 1px #7b7b7b;
+            border-radius: 1rem;
+            text-align: center;
+            line-height: 2rem;
+            user-select: none;
+            cursor: pointer;
+        }
+
+        .configure-default {
+            @extend %configure-btn;
+            color: #7b7b7b;
+            background-color: #f4f4f4;
+        }
+
+        .configure-active {
+            @extend %configure-btn;
+            color: #f4f4f4;
+            background-color: #7b7b7b;
+        }
+
+        .configure-box {
+            position: relative;
+            width: 100%;
+            border-radius: 1rem;
+            box-sizing: border-box;
+            background: #f4f4f4;
+            color: #7b7b7b;
+            overflow: hidden;
+            transition: all 0.5s;
+
+            .conf-line {
+                position: relative;
+                width: calc(100% - 2rem);
+                height: 3rem;
+                padding: 0 1rem;
+                display: flex;
+                align-items: center;
+                justify-content: flex-start;
+
+                .conf-label {
+                    position: relative;
+                    width: 10rem;
+                    font-weight: bold;
+                }
+
+                .color-picker {
+                    border: none;
+                    outline: none;
+                    cursor: pointer;
+                }
+
+                .slider {
+                    -webkit-appearance: none;
+                    height: 0.5rem;
+                    border: solid 1px #7b7b7bcc;
+                    border-radius: 0.25rem;
+                    background-color: #f4f4f4;
+
+                    &::-webkit-slider-thumb {
+                        -webkit-appearance: none;
+                        width: 1rem;
+                        height: 1rem;
+                        border-radius: 0.5rem;
+                        background: #7b7b7b80;
+                        cursor: pointer;
+                    }
+                }
+
+                .radios {
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-start;
+
+                    %radio {
+                        position: relative;
+                        width: calc(5rem - 2px);
+                        height: calc(2rem - 2px);
+                        border: solid 1px #7b7b7bcc;
+                        user-select: none;
+                        cursor: pointer;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    }
+
+                    .radio-default {
+                        @extend %radio;
+                        color: #7b7b7b;
+                        background-color: #f4f4f4;
+                    }
+
+                    .radio-active {
+                        @extend %radio;
+                        color: #f4f4f4;
+                        background-color: #7b7b7b;
+                    }
+                }
+            }
+        }
+    }
 
     .fake-btn {
         position: relative;
